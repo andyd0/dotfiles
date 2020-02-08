6d387fb2cdb92ae804470a5b04414f81a78c580a
Andy Davila <andy.davila@gmail.com>
2020-02-08 10:43:59 -0500

    Add snapshot hammerspoon shortcuts

--- hammerspoon/misc.lua
+++ hammerspoon/misc.lua
@@ -2,13 +2,21 @@ hs.hotkey.bind(hyper, "w", function() hs.eventtap.keyStrokes('¯\\_(ツ)_/¯'); 
 hs.hotkey.bind(hyper, "a", function() hs.caffeinate.lockScreen(); end)
 
 hs.hotkey.bind(hyper, "`", function() 
-  local timeStamp = os.date("%d-%b-%Y")
-  hs.eventtap.keyStrokes(timeStamp)
+  hs.eventtap.keyStrokes(os.date("%d-%b-%Y"))
 end)
 
+function getTimeStamp()
+  return string.gsub(os.date("%Y-%m-%d_%T"), ":", ".")
+end
+
 hs.hotkey.bind(hyper, "4", function()
-  local timeStamp = string.gsub(os.date("%Y-%m-%d_%T"), ":", ".")
-  local fileName = os.getenv("HOME") .. "/Desktop/ss-" .. timeStamp .. ".png"
+  local fileName = os.getenv("HOME") .. "/Desktop/ss-" .. getTimeStamp() .. ".png"
+  hs.task.new("/usr/sbin/screencapture", nil, {"-i", fileName }):start()
+end)
+
+hs.hotkey.bind(hyper, "5", function()
+  local fileName = os.getenv("HOME") .. "/Desktop/ss-" .. getTimeStamp() .. ".png"
   local windowId = hs.window.frontmostWindow():id()
   hs.task.new("/usr/sbin/screencapture", nil, {"-l" .. windowId, fileName }):start()
 end)
+
